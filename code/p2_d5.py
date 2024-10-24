94455e0a0abf26bc5a9485682567bdaaa435914d
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-10-24 16:53:52 -0500

    Add new def, some references

---
+++ code/p2_d5.py
@@ -0,0 +1,18 @@
+from itertools import count
+from typing import Iterator
+
+from .args import run
+
+
+def seq_p2_d5(_: int = 2) -> Iterator[int]:
+    value = 1
+    for n in count():
+        # Note: assumes that (-1).bit_length() gives 1
+        x = (n ^ (n - 1)).bit_length() + 1
+        if x & 1 == 0:
+            value = 1 - value  # Bit index is even, so toggle value
+        yield value
+
+
+if __name__ == '__main__':
+    args = run(5, seq_p2_d5, '2')

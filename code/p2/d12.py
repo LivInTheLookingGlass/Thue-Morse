832732b3602d61f4ca2da2ee45be266ff2e0eb3f
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-10-31 21:46:01 -0500

    Attempt simpler implementation of p2d12. If that doesn't work, will parallelize

--- code/p2/d12.py
+++ code/p2/d12.py
@@ -1,26 +1,16 @@
-from itertools import count
-from typing import Iterator
+from itertools import tee
+from typing import Iterator, Tuple
 
 from ..args import run
 
 
-def gould(n: int) -> int:
-    binomial_coeff = 1
-    partial_sum = 0
-    for k in range((n + 1) // 2):
-        # Add the current term to the total sum
-        partial_sum += binomial_coeff % 2
-        # C(n, k) = C(n, k-1) * (n - (k - 1)) / k
-        binomial_coeff = binomial_coeff * (n - k) // (k + 1)
-    partial_sum *= 2
-    if n % 2 == 0:
-        partial_sum += binomial_coeff % 2
-    return partial_sum
-
-
 def p2_d12(_: int = 2) -> Iterator[int]:
-    for i in count():
-        yield (gould(i) - 1) % 3
+    row: Tuple[int, ...] = (1, )
+    while True:
+        yield (sum(x % 2 for x in row) - 1) % 3
+        it1, it2 = tee(iter(row), 2)
+        next(it2)
+        row = (1, *(x + y for x, y in zip(it1, it2)), 1)
 
 
 if __name__ == '__main__':

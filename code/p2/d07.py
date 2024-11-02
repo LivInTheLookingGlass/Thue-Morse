bb9de231cfeb1d9d8acf2e7ed7cd1ab706038b2b
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-01 22:40:15 -0500

    add memoization memory limit

--- code/p2/d07.py
+++ code/p2/d07.py
@@ -6,16 +6,20 @@ from ..args import run
 
 
 def b(n: int, memo: List[Optional[int]]) -> int:
-    if memo[n] is None:
-        n2 = n / 2
-        memo[n] = b(ceil(n2), memo) - b(floor(n2), memo)
-    return memo[n]
+    if n < len(memo) and memo[n] is not None:
+        return memo[n]
+    n2 = n / 2
+    result = b(ceil(n2), memo) - b(floor(n2), memo)
+    if n < len(memo):
+        memo[n] = result
+    return result
 
 
 def p2_d07(_: int = 2) -> Iterator[int]:
     memo: List[Optional[int]] = [0, 1]
     for i in count(1):
-        memo.extend((None, None))
+        if i < (1 << 20):
+            memo.extend((None, None))
         yield (1 - b(2 * i - 1, memo)) // 2
 
 

e323fd28384e6ccef68a2e99344ec62d818db5c0
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-01 22:35:47 -0500

    switch from cache to memoization

--- code/p2/d07.py
+++ code/p2/d07.py
@@ -1,22 +1,22 @@
-from functools import cache
 from itertools import count
 from math import ceil, floor
-from typing import Iterator
+from typing import Iterator, List, Optional
 
 from ..args import run
 
 
-@cache
-def b(n: int) -> int:
-    if n <= 1:
-        return n
-    n2 = n / 2
-    return b(ceil(n2)) - b(floor(n2))
+def b(n: int, memo: List[Optional[int]]) -> int:
+    if memo[n] is None:
+        n2 = n / 2
+        memo[n] = b(ceil(n2), memo) - b(floor(n2), memo)
+    return memo[n]
 
 
 def p2_d07(_: int = 2) -> Iterator[int]:
+    memo: List[Optional[int]] = [0, 1]
     for i in count(1):
-        yield (1 - b(2 * i - 1)) // 2
+        memo.extend((None, None))
+        yield (1 - b(2 * i - 1, memo)) // 2
 
 
 if __name__ == '__main__':

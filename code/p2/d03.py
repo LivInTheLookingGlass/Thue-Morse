9e35aa579b5e5002158761c3db25804783cebec0
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-03 15:25:22 -0600

    Fix p2_d3; switch matrix definitions

--- code/p2/d03.py
+++ code/p2/d03.py
@@ -10,7 +10,7 @@ def p2_d03(_: int = 2) -> Iterator[int]:
     seq = bitarray((0, 1))
     prev_len = 0
     while True:
-        yield from islice(chain.from_iterable(seq), prev_len, None)
+        yield from islice(seq, prev_len, None)
         prev_len = len(seq)
         seq = bitarray(chain.from_iterable(range(1, -1, -1) if x else range(2) for x in seq))
 

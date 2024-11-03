2ca28049c7ba8674464f131a0c1d6d4507058d2d
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-03 11:49:34 -0600

    Optimize d4 for memory usage

--- code/pn/d04.py
+++ code/pn/d04.py
@@ -1,17 +1,28 @@
+from ctypes import c_uint8
 from itertools import chain, islice
 from typing import Iterator, Tuple
 
 from ..args import run
 from ..p2.d04 import rotate
+from ..pn.d02 import fill_ctypes_array
 
 
 def pn_d04(n: int = 2) -> Iterator[int]:
-    seq: Tuple[int, ...] = tuple(range(n))
+    capacity = n
+    seq = (c_uint8 * capacity)()
+    fill_ctypes_array(seq, range(n), n)
     prev_len = 0
     while True:
         yield from islice(seq, prev_len, None)
-        prev_len = len(seq)
-        seq = (*chain.from_iterable(rotate(seq, prev_len // n * i) for i in range(n)), )
+        prev_len = capacity
+        capacity *= n
+        new_seq = (c_uint8 * capacity)()
+        fill_ctypes_array(
+            new_seq,
+            chain.from_iterable(rotate(seq, prev_len // n * i) for i in range(n)),
+            capacity
+        )
+        seq = new_seq
 
 
 if __name__ == '__main__':

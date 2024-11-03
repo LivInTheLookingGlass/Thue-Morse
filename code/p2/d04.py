4ff90e0ab07c44b63659b6e54042c1b6be2a8103
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-03 11:20:57 -0600

    Optimize d4 for memory usage

--- code/p2/d04.py
+++ code/p2/d04.py
@@ -1,15 +1,19 @@
 from itertools import islice
-from typing import Iterator, Tuple
+from typing import Iterator, Sequence, TypeVar
+
+from bitarray import bitarray
 
 from ..args import run
 
+T = TypeVar("T")
 
-def rotate(t: Tuple[int, ...], n: int) -> Tuple[int, ...]:
+
+def rotate(t: Sequence[T], n: int) -> Sequence[T]:
     return n and (*t[n:], *t[:n]) or t
 
 
 def p2_d04(_: int = 2) -> Iterator[int]:
-    seq: Tuple[int, ...] = (0, 1)
+    seq: bitarray = (0, 1)
     prev_len = 0
     while True:
         yield from islice(seq, prev_len, None)

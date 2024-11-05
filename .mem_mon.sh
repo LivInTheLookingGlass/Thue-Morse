917bb169703bbf0b8f049fef221b530978c182e1
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-05 04:46:03 -0600

    Monitor memory usage during testing (2)

--- .mem_mon.sh
+++ .mem_mon.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 monitor_memory() {
   min_free_mem=999999999  # Arbitrarily large number to track the minimum
+  handle_exit() {
+    echo "Lowest free memory seen during the pytest run: $min_free_mem MB"
+    exit 0  # Exit the script cleanly
+  }
+  trap handle_exit SIGTERM
   while true; do
     free_mem=$(free -m | awk '/^Mem:/ {print $4}')  # Get free memory in MB
     if [[ $free_mem -lt $min_free_mem ]]; then

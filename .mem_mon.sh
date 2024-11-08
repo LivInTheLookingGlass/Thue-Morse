a3d2f64f5dedc9e87b399f4cb21056a14d68a26b
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-08 00:58:19 -0600

    expand memory limit using a swapfile; adjust test bounds accordingly

--- .mem_mon.sh
+++ .mem_mon.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 monitor_memory() {
-  min_free_mem=999999999  # Arbitrarily large number to track the minimum
+  min_free_mem_swap=999999999  # Arbitrarily large number to track the minimum
   handle_exit() {
-    echo "Lowest free memory seen during the pytest run: $min_free_mem MB"
+    echo "Lowest combined free memory + swap seen during the pytest run: $min_free_mem_swap MB"
     exit 0  # Exit the script cleanly
   }
   trap handle_exit SIGTERM
   while true; do
     free_mem=$(free -m | awk '/^Mem:/ {print $4}')  # Get free memory in MB
-    if [[ $free_mem -lt $min_free_mem ]]; then
-      min_free_mem=$free_mem  # Update if we found a new minimum
+    free_swap=$(free -m | awk '/^Swap:/ {print $4}')  # Get free swap in MB
+
+    total_free_mem_swap=$((free_mem + free_swap))  # Sum of free memory and swap
+
+    if [[ $total_free_mem_swap -lt $min_free_mem_swap ]]; then
+      min_free_mem_swap=$total_free_mem_swap  # Update if we found a new minimum combined value
     fi
-    sleep 5  # Poll every 5 seconds
+
+    sleep 2
   done
 }

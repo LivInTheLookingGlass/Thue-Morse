9ffe3ad0dc07b209f16bb54039b94c1feb891995
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-07 18:17:09 -0600

    Optimize 2_15

--- dev/p2/d15.py
+++ dev/p2/d15.py
@@ -1,27 +1,23 @@
 from typing import Iterator
 
-from sympy import symbols
+from sympy import expand, symbols
 
 from ..args import run
 
 
-def gen_inf_prod_approx(x, k_max):
-    product_term = 1
-    for k in range(k_max):
-        product_term *= (1 - x**(2**k))
-    return product_term
-
-
 def p2_d15(_: int = 2) -> Iterator[int]:
     n = 1
+    k = 1
     x = symbols('x')
-    simple_gf = 1 / (1 - x)
+    simple_gf = expand(1 / (1 - x))
+    approx_prod = 1 - x
     while True:
-        approx_prod = gen_inf_prod_approx(x, n.bit_length() - 1)
+        approx_prod *= (1 - x**(2**k))
         gen_func = (simple_gf - approx_prod) / 2
-        series_expansion = gen_func.series(x, 0, n).removeO()  # Remove the Big-O term
+        series_expansion = expand(gen_func.series(x, 0, n).removeO())
         yield from (series_expansion.coeff(x, i) for i in range(n >> 1, n))
         n <<= 1
+        k += 1
 
 
 if __name__ == '__main__':

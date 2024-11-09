db2df54d6564b56700153053f50c0692b7c7f43c
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-08 18:52:14 -0600

    Small optimization to d15; change ranges

--- dev/p2/d15.py
+++ dev/p2/d15.py
@@ -12,7 +12,7 @@ def p2_d15(_: int = 2) -> Iterator[int]:
     simple_gf = expand(1 / (1 - x))
     approx_prod = 1 - x
     while True:
-        approx_prod *= (1 - x**(2**k))
+        approx_prod *= (1 - x**(1 << k))
         gen_func = (simple_gf - approx_prod) / 2
         series_expansion = expand(gen_func.series(x, 0, n).removeO())
         yield from (series_expansion.coeff(x, i) for i in range(n >> 1, n))

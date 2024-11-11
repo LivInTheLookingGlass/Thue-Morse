f98900629da7af5d03a1c467c08703e8f61c4a20
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-10 22:05:21 -0600

    attempt int coersion

--- dev/p2/d02.py
+++ dev/p2/d02.py
@@ -2,7 +2,7 @@ from itertools import count
 from typing import Dict, Iterator, Union
 
 try:
-    from z3 import Function, If, Int, IntSort, RecAddDefinition, RecFunction
+    from z3 import Function, If, Int, IntSort, RecAddDefinition, RecFunction, ToInt
 except ImportError:
     pass
 
@@ -18,7 +18,7 @@ def to_z3(_: Union[int, 'Int'] = 2) -> Dict[str, Union['Function', 'RecFunction'
     n = Int('n')
     p = RecFunction('p', IntSort(), IntSort())
     RecAddDefinition(p, [n], If(n == 0, 0, (p(n / 2) + (n % 2))))
-    T2_02 = (1 - (-1)**p(n)) / 2
+    T2_02 = ToInt((1 - (-1)**p(n)) / 2)
     return {
         'p': p,
         'T': T2_02

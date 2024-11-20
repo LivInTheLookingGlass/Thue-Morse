d848eda0a215b46301fa8d858434794fcd8f3aae
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-11-19 23:16:52 -0600

    Fix some type errors in file writing

--- dev/pn/d09.py
+++ dev/pn/d09.py
@@ -2,7 +2,7 @@ from functools import reduce
 from operator import mul
 from typing import Iterator
 
-from sympy import I, exp, expand, log, pi, simplify, summation, symbols
+from sympy import I, exp, expand, log, pi, symbols
 
 from ..args import run
 from ..compat.fluidpythran import boost
@@ -18,20 +18,20 @@ def pn_d09(s: int = 2) -> Iterator[int]:
     i = symbols('i')
     log_omega_s = 2 * pi * I / s
     omega_s = exp(log_omega_s)
-    gen_func = simplify(reduce(mul, (
-        (summation(omega_s**i * x**(s**k * i), (i, 0, s - 1))) for k in range(start)
-    )))
+    gen_func = reduce(mul, (
+        sum(omega_s**i * x**(s**k * i) for i in range(s)) for k in range(start)
+    ))
     while True:
-        gen_func *= simplify(summation(omega_s**i * x**(s**k * i), (i, 0, s - 1)))
+        gen_func *= sum(omega_s**i * x**(s**k * i) for i in range(s))
         series_expansion = expand(gen_func.series(x, 0, n).removeO())
         for index in range(nts, n):
             coeff = series_expansion.coeff(x, index)
             if coeff == 0:
-                yield coeff
+                yield 0
                 continue
             power = log(coeff) / log_omega_s % s
             if power.is_integer:
-                yield power
+                yield int(power)
             else:
                 yield round(power.evalf())  # just in case there is floating point drift
         nts = n

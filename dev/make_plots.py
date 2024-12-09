37240b88cdabed0de8a643b54e92ebed2bab80b0
Olivia Appleton <olivia.kay.appleton@gmail.com>
2024-12-08 20:04:49 -0600

    mypy compliance

--- dev/make_plots.py
+++ dev/make_plots.py
@@ -1,12 +1,14 @@
 from collections import defaultdict
 from itertools import product
 from math import log10, log2
+from typing import Callable, Dict, List, Optional, Tuple, cast
 
-import matplotlib.pyplot as plt
+from matplotlib import colormaps as cm
+from matplotlib import pyplot as plt
 from numpy import linspace
 
 
-time_map = {
+time_map: Dict[str, float] = {
     r'1':                                     1,
     r'\log(n)':                               2,
     r'n':                                     100,
@@ -22,7 +24,7 @@ time_map = {
     r'n^{\log_2(3)} \cdot \log(n)':           100**log2(3) * 2,
 }
 
-space_map = {
+space_map: Dict[str, float] = {
     r'1':                 1,
     r'n':                 100,
     r'\log(n)':           2,
@@ -35,47 +37,50 @@ space_map = {
 reverse_time_map = {v: k for k, v in time_map.items()}
 reverse_space_map = {v: k for k, v in space_map.items()}
 
-get_time_value = time_map.get
-get_space_value = space_map.get
+get_time_value = cast(Callable[[Optional[str]], Optional[float]], time_map.get)
+get_space_value = cast(Callable[[Optional[str]], Optional[float]], space_map.get)
 get_time_complexity = reverse_time_map.get
 get_space_complexity = reverse_space_map.get
 
 # name, times, spaces
 # fixed+single, fixed+total, flex+single, flex+total
-complexities = [
+complexities: List[Tuple[
+    str,
+    Tuple[Optional[str], Optional[str], Optional[str], Optional[str]],
+    Tuple[Optional[str], Optional[str], Optional[str], Optional[str]]]] = [
     ('$T_{2,1}$',
-     [r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2 \cdot \log(\log(n))',       r'n \cdot \log(n)^2 \cdot \log(\log(n))'],
-     [r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)']),
+     (r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2 \cdot \log(\log(n))',       r'n \cdot \log(n)^2 \cdot \log(\log(n))'),
+     (r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)')),
     ('$T_{2,4}$',
-     [r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2',                           r'n \cdot \log(n)^2 \cdot \log(\log(n))'],
-     [r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)']),
-    ('$T_{2,5a}$',
-     [r'n',                           r'n^2',                         r'n \cdot \log(n) \cdot \log(\log(n))', r'n^2 \cdot \log(n) \cdot \log(\log(n))'],
-     [r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2',                           r'n \cdot \log(n)^2']),
-    ('$T_{2,5b}$',
-     [r'n',                           r'n^2',                         r'n \cdot \log(n) \cdot \log(\log(n))', r'n^2 \cdot \log(n) \cdot \log(\log(n))'],
-     [r'n',                           r'n^2',                         r'n \cdot \log(n)',                     r'n^2 \cdot \log(n)']),
+     (r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2',                           r'n \cdot \log(n)^2 \cdot \log(\log(n))'),
+     (r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)')),
+    ('$T_{2,5r}$',
+     (r'n',                           r'n^2',                         r'n \cdot \log(n) \cdot \log(\log(n))', r'n^2 \cdot \log(n) \cdot \log(\log(n))'),
+     (r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2',                           r'n \cdot \log(n)^2')),
+    ('$T_{2,5d}$',
+     (r'n',                           r'n^2',                         r'n \cdot \log(n) \cdot \log(\log(n))', r'n^2 \cdot \log(n) \cdot \log(\log(n))'),
+     (r'n',                           r'n^2',                         r'n \cdot \log(n)',                     r'n^2 \cdot \log(n)')),
     ('$T_{2,6}$',
-     [r'n \cdot \log(n)',             r'n \cdot \log(n)',             r'n \cdot \log(n)^2',                   r'n \cdot \log(n)^2'],
-     [r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)']),
+     (r'n \cdot \log(n)',             r'n \cdot \log(n)',             r'n \cdot \log(n)^2',                   r'n \cdot \log(n)^2'),
+     (r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)')),
     ('$T_{2,15s}$',
-     [r'n',                           r'n^2',                         r'n',                                   r'n^2'],
-     [r'n',                           r'n^2',                         r'n \cdot \log(n)',                     r'n^2 \cdot \log(n)']),
+     (r'n',                           r'n^2',                         r'n',                                   r'n^2'),
+     (r'n',                           r'n^2',                         r'n \cdot \log(n)',                     r'n^2 \cdot \log(n)')),
     ('$T_{2,15p}$',
-     [r'n',                           r'n^2',                         r'n \cdot \log(n) \cdot \log(\log(n))', r'n^2 \cdot \log(n) \cdot \log(\log(n))'],
-     [r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)']),
+     (r'n',                           r'n^2',                         r'n \cdot \log(n) \cdot \log(\log(n))', r'n^2 \cdot \log(n) \cdot \log(\log(n))'),
+     (r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)')),
     ('$T_{2,17}$',
-     [r'n^{\log_2(3)} \cdot \log(n)', r'n^{\log_2(3)} \cdot \log(n)', None,                                   None],
-     [r'n',                           r'n',                           r'n \cdot \log(n)',                     r'n \cdot \log(n)']),
+     (r'n^{\log_2(3)} \cdot \log(n)', r'n^{\log_2(3)} \cdot \log(n)', None,                                   None),
+     (r'n',                           r'n',                           r'n \cdot \log(n)',                     r'n \cdot \log(n)')),
     ('$T_{n,1}$',
-     [r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2 \cdot \log(\log(n))',       r'n \cdot \log(n)^2 \cdot \log(\log(n))'],
-     [r'1',                           r'n',                           r'\log(n)',                             r'\n \cdot \log(n)']),
+     (r'\log(n)',                     r'n \cdot \log(n)',             r'\log(n)^2 \cdot \log(\log(n))',       r'n \cdot \log(n)^2 \cdot \log(\log(n))'),
+     (r'1',                           r'n',                           r'\log(n)',                             r'\n \cdot \log(n)')),
     ('$T_{n,3}$',
-     [r'\log(n)',                     r'n \cdot \log(n)',             r'n \cdot \log(n)',                     r'n \cdot \log(n)^2'],
-     [r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)']),
+     (r'\log(n)',                     r'n \cdot \log(n)',             r'n \cdot \log(n)',                     r'n \cdot \log(n)^2'),
+     (r'1',                           r'n',                           r'\log(n)',                             r'n \cdot \log(n)')),
     ('$T_{n,9}$',
-     [r'\log(n)',                     r'\log(n)',                     None,                                   None],
-     [r'n',                           r'n',                           r'n \cdot \log(n)',                     r'n \cdot \log(n)']),
+     (r'\log(n)',                     r'\log(n)',                     None,                                   None),
+     (r'n',                           r'n',                           r'n \cdot \log(n)',                     r'n \cdot \log(n)')),
 ]
 
 
@@ -92,14 +97,16 @@ for is_arb, is_total in product(range(2), range(2)):
         #     raise ValueError()
 
     num_entries = sum(len(names) for names in groups.values())
-    colors = plt.cm.plasma(linspace(0, 1, num_entries))
+    colors = cm['plasma'](linspace(0, 1, num_entries))
 
     plt.figure(figsize=(8, 6))
 
     for idx, ((time_value, space_value), names) in enumerate(groups.items()):
         color = colors[idx]
         plt.scatter(time_value, space_value, color=color)
-        group_label = ', '.join(names) + f": ($O({get_time_complexity(time_value)})$, $O({get_space_complexity(space_value)})$)"
+        tc = get_time_complexity(time_value)
+        sc = get_space_complexity(space_value)
+        group_label = ', '.join(names) + f": ($O({tc})$, $O({sc})$)"
         plt.plot([], [], 'o', color=color, label=group_label)
 
     plt.xscale('log')
@@ -108,8 +115,8 @@ for is_arb, is_total in product(range(2), range(2)):
     time_ticks = sorted(set(time_map.values()))
     space_ticks = sorted(set(space_map.values()))
 
-    time_labels = ['$' + get_time_complexity(tick) + '$' for tick in time_ticks]
-    space_labels = ['$' + get_space_complexity(tick) + '$' for tick in space_ticks]
+    time_labels = ['$' + cast(str, get_time_complexity(tick)) + '$' for tick in time_ticks]
+    space_labels = ['$' + cast(str, get_space_complexity(tick)) + '$' for tick in space_ticks]
 
     plt.xlim(min(time_ticks) * 0.9, max(time_ticks) * 1.1)
     plt.ylim(min(space_ticks) * 0.9, max(space_ticks) * 1.1)
